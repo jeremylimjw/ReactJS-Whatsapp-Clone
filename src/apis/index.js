b2d64634fc879bf2da8db5bde56bf88a2a9bc49b
Jeremy <jeremylimjw@gmail.com>
2022-04-08 16:55:29 +0800

    Changed http cookies to autorization headers

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,5 +2,15 @@ import axios from 'axios';
 
 export const axiosWrapper = axios.create({
     baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
-    withCredentials: true,
+    withCredentials: true
 });
+
+export function getAuthorizationHeader() {
+    const userId = JSON.parse(sessionStorage.getItem("user"))?._id;
+    const authorization = {
+        headers: {
+            'Authorization': userId 
+        }
+    }
+    return authorization;
+}
